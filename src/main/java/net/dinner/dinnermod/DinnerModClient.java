471cae2f37eebdf08f5a58c92a20726caa1fe0d9
Hrrfee <Harrafeebruh@gmail.com>
2025-01-20 06:39:23 +0700

    Initial commit

---
+++ src/main/java/net/dinner/dinnermod/DinnerModClient.java
@@ -0,0 +1,8 @@
+package net.dinner.dinnermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DinnerModClient implements ClientModInitializer {
+    @Override
+    public void onInitialize()
+}
---
+++ src/main/java/net/dinner/dinnermod/DinnerModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dinner.dinnermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DinnerModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
